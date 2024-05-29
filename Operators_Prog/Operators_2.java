15e8ac48b8fddc31921c382116500f621af34314
Veeresh8310 <152872684+Veeresh8310@users.noreply.github.com>
2024-05-29 21:09:37 +0530

    Add files via upload

---
+++ Operators_Prog/Operators_2.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=x++;
+		System.out.println(y);
+	}
+
+}
---
+++ Operators_Prog/operators_1.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		int c=a+b;
+		System.out.println(c);
+		String s="a";
+		System.out.println(5.0f/4);
+  char d='a';
+  d+=1;
+  System.out.println(d+2);
+	}
+
+}
