4099b5d9e48764137e8ec32ef56908eb1e049cfa
Veeresh8310 <152872684+Veeresh8310@users.noreply.github.com>
2024-06-04 21:31:54 +0530

    Add files via upload

---
+++ Pattern_Prog/pattern1.java
@@ -0,0 +1,20 @@
+package scanner;
+
+public class pri {
+
+	public static void main(String[] args) {
+//		int l=5;
+		for(int i=6;i>1;i--)
+		{
+			for(int j=i;j>1;j--)
+			{
+			System.out.print("*");
+			}
+			System.out.println();
+//			l--;
+		}
+		
+
+	}
+
+}
