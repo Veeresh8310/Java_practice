463939ab8ae3befa24ac31c1da794bc85cc74177
Veeresh8310 <152872684+Veeresh8310@users.noreply.github.com>
2024-06-09 00:47:08 +0530

    Add files via upload

---
+++ JDBC/book_project/bookcrud.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import java.util.Scanner;
+
+public class bookcrud {
+
+	public static void main(String[] args) {
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the ");
+		
+
+	}
+
+}
